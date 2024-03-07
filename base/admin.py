62a9f6f35218ca169125b8e39d6e60d425f6ed24
Vadym Kurdydyk <vadimkurdidik@gmail.com>
2024-03-07 10:41:48 +0200

    Added Crud

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .models import Room, Food
+from .models import Room, Food, SportCourse, Topic, Message
 
 admin.site.register(Food)
 admin.site.register(Room)
+admin.site.register(SportCourse)
+admin.site.register(Topic)
+admin.site.register(Message)
--- base/views.py
+++ base/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
+from .forms import RoomForm
+
+
+def loginPage(request):
+    context = {}
+    return render(request, r'base/login-register.html', context)
 
 def room(request,pk):
     room = Room.objects.get(id = pk )
@@ -9,3 +15,35 @@ def room(request,pk):
 def home(request):
     rooms = Room.objects.all()
     return render(request, r'base/home.html',{'rooms': rooms })
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == "POST":
+        form = RoomForm(request.POST)
+        print(request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect("home")
+    context = {"form": form}
+    return render(request, r'base/room_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance = room)
+
+    if request.method == "POST":
+        form = RoomForm(request.POST, instance= room)
+    if form.is_valid():
+        form.save()
+        return redirect("home")
+    
+    context = {"form": form}
+    return render(request, r'base/room_form.html', context)
+
+def deleteRoom(request,pk):
+    room = Room.objects.get(id=pk)
+    if request.method == "POST":
+        room.delete()
+        return redirect("home")
+    context = {'obj':room}
+    return render(request, r'base/delete.html', context)
