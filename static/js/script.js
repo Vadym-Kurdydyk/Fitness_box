b3690b0e7993b42563ecda7032ab50b903a8acf9
Vadym Kurdydyk <vadimkurdidik@gmail.com>
2024-05-03 15:43:08 +0300

    Final touch to the light theme
    Made it so when you refresh the page light theme does not change to dark and vice versa. It was a serious issue when typing a message or changing to the new page

--- static/js/script.js
+++ static/js/script.js
@@ -29,9 +29,26 @@ if (conversationThread) {
 
 // Switch theme
 const themeToggle = document.getElementById('theme-toggle');
+const themeStyle = document.getElementById('theme-style');
+const themeToggleCheckbox = document.getElementById('theme-toggle');
 
-themeToggle.addEventListener('click', () => {
-  document.documentElement.classList.toggle('light');
-});
+
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme) {
+  document.documentElement.classList.add(savedTheme);
+  if (savedTheme === 'light') {
+    themeToggleCheckbox.checked = true;
+  }
+}
+
+
+function toggleTheme() {
+  const currentTheme = themeToggleCheckbox.checked ? 'light' : 'dark';
+  localStorage.setItem('theme', currentTheme);
+  document.documentElement.classList.toggle('light', currentTheme === 'light');
+}
+
+
+themeToggle.addEventListener('click', toggleTheme);
 
 
