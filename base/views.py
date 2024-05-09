9fff54e7af0e345556810a97bd0cf255bb2a5ef8
Vadym Kurdydyk <vadimkurdidik@gmail.com>
2024-05-09 18:37:25 +0300

    2

--- base/views.py
+++ base/views.py
@@ -72,17 +72,23 @@ def room(request, pk):
     participants = room.participants.all()
 
     if request.method == 'POST':
-        message = Message.objects.create(
+      createMessage(request,pk)
+    context = {'room': room, 'room_messages': room_messages,
+               'participants': participants}
+    return render(request, 'base/room.html', context)
+
+@login_required(login_url='login')
+def createMessage(request,pk):
+    room = Room.objects.get(id=pk)
+    room_messages = room.message_set.all()
+    participants = room.participants.all()
+    message = Message.objects.create(
             user=request.user,
             room=room,
             body=request.POST.get('body')
         )
-        room.participants.add(request.user)
-        return redirect('room', pk=room.id)
-
-    context = {'room': room, 'room_messages': room_messages,
-               'participants': participants}
-    return render(request, 'base/room.html', context)
+    room.participants.add(request.user)
+    return redirect('room', pk=room.id)
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
